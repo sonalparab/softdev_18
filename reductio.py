c12e3f77af46bb45c4d45437016da88ec31cc86d
Jake Goldman <jgoldman@stuy.edu>
2018-05-01 09:29:03 -0400

    made it faster

--- reductio.py
+++ reductio.py
@@ -5,6 +5,9 @@ f.close()
 book = book.split(" ")
 book = [0] + book
 
+
+# BECAUSE THE PURPOSE OF THE ASSIGNMENT IS TO WORK WITH LIST COMPHREHENSION/REDUCE,
+# WE'RE IGNORING THE INACCURACY THAT PUNCTUATION/UPPER CASE LETTERS WILL CAUSE
 def freq(word):
     return reduce(lambda a,b: a+1 if b == word else a,book)
 
@@ -27,10 +30,19 @@ print "Group frequency of 'the' and 'a' and 'letter'"
 print group_freq(["the","a","letter"])
 print
 
+
 def most_freq():
-    return reduce(lambda a,b: a if a != b and freq(a) > freq(b) else b,{w for w in book})
+    # takes too long to use dictionary comprehension...
+    # count_dict = { key: freq(key) for key in book }
+    count_dict = dict()
+    for word in book:
+        if word in count_dict:
+            count_dict[word] += 1
+        else:
+            count_dict[word] = 1
+    return reduce( lambda a,b: a if count_dict[a] > count_dict[b] else b, count_dict.keys() )
+    # return reduce(lambda a,b: a if a != b and freq(a) > freq(b) else b,book)
 
 print "Most frequent word"
+# print "This will take a long time to run, the result should be 'the'..."
 print most_freq()
-
-
