3ae528e7d30ddff348c43b07e34dc3fc68a02601
sonalparab <sparab@stuy.edu>
2018-04-30 20:35:36 -0400

    added the remaining functions

--- reductio.py
+++ reductio.py
@@ -2,12 +2,35 @@ f = open('book.txt','rU')
 book = f.read()
 f.close()
 
-list = book.split(" ")
-list[0] = 0
+book = book.split(" ")
+book = [0] + book
 
 def freq(word):
-    return reduce(lambda a,b: a+1 if b == word else a,list)
+    return reduce(lambda a,b: a+1 if b == word else a,book)
 
-print freq("letter")
+print "Frequency of 'the'"
 print freq("the")
+print "Frequency of 'a'"
+print freq("a")
+print "Frequency of 'letter'"
+print freq("letter")
+print
+
+def group_freq(words):
+    return reduce(lambda a,b: a+b,[freq(a) for a in words])
+
+print "Group frequency of 'the' and 'letter'"
+print group_freq(["the","letter"])
+print "Group frequency of 'the' and 'a'"
+print group_freq(["the","a"])
+print "Group frequency of 'the' and 'a' and 'letter'"
+print group_freq(["the","a","letter"])
+print
+
+def most_freq():
+    return reduce(lambda a,b: a if a != b and freq(a) > freq(b) else b,book)
+
+print "Most frequent word"
+print most_freq()
+
 
